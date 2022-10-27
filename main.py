89bf454b1514e7af7992e2d4d8c06919221f369e
RVEGA <richard.vega.08@gmail.com>
2022-10-27 15:29:45 -0500

    Condicionales

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numero1= int(input("Ingrese el Primero numero "))
+""" numero1= int(input("Ingrese el Primero numero "))
 numero2= int(input("Ingrese el segundo numero "))
 
-print("La Suma de Dos numeros es ", numero1 + numero2)
+print("La Suma de Dos numeros es ", numero1 + numero2) """
+
+promedio=int(input("Ingresar nota"))
+
+if promedio >7:
+    print("Aprobado")
+else:
+    print("DEsaprobado")
